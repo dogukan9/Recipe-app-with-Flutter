ae0559478ea21d8794e04e76febb21dc9c83cc37
Doğukan Öncüer <dogukanoncuer@Dogukan-MacBook-Air.local>
2022-03-20 18:21:49 +0300

    first commit

---
+++ lib/category_item.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+ final String title;
+ final Color color;
+ final String id;
+
+ CategoryItem(this.id,this.title,this.color);
+
+ void selectCategory(BuildContext ctx){
+   Navigator.of(ctx).push(MaterialPageRoute(builder:(_){
+     return CategoryMealsScreen(id,title);
+   },),);
+
+ }
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      onTap:()=>selectCategory(context) ,
+      child: Container(
+      padding:const EdgeInsets.all(15),
+      child: Text(title,style: Theme.of(context).textTheme.bodyText1),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors:[color.withOpacity(0.7),color.withOpacity(1)],
+          begin: Alignment.topLeft,
+          end:Alignment.bottomRight,
+
+          ),
+          borderRadius: BorderRadius.circular(15)
+          ),
+    )
+    );
+  }
+}
